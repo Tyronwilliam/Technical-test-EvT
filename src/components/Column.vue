4f8f3fabac4d3092e57bed2e3faac0b235d3b16d
Tyron William CHANU <tyronwilliam.chanu@veepee.com>
2025-07-29 12:31:10 +0200

    chore(Columns) add comment to identify column div

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -10,6 +10,7 @@ defineProps<{
       class="text-center inline-block w-full font-bold rounded-t-md border-t-2 border-l-2 border-r-2 py-4 border-gray-900 bg-slate-700"
       >{{ title }}</span
     >
+    <!-- Columns -->
     <div
       class="flex lg:flex-col gap-4 p-2 max-h-[600px] overflow-y-scroll border-2 border-gray-900 rounded-b-md shadow-2xl bg-slate-700"
     >
